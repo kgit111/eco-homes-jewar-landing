a953165eb951c046fa6b39efe0b33c2c7729de57
VRINDA_LAPTOP\karan <kbat312@gmail.com>
2025-05-23 20:45:56 +0530

    adding yamunaecohomes domain to package json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/eco-homes-jewar-landing/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
